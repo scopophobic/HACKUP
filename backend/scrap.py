e0ba448a9c103c21a12c8c4440e0085e7b09ea4d
scopophobic <adithyanmadhu1234@hmail.com>
2023-04-11 05:19:40 +0530

    scraped data

--- backend/scrap.py
+++ backend/scrap.py
@@ -3,39 +3,42 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import time
 
-# Set up the webdriver
-driver = webdriver.Chrome(
+
+def unstophackathons():
+    driver = webdriver.Chrome(
     '/Users/scopo0/Downloads/chromedriver_mac_arm64/chromedriver')
+    # url = 'https://unstop.com/hackathons'
+    driver.get('https://unstop.com/hackathons')
 
-# Load the webpage
-driver.get('https://unstop.com/hackathons')
+    # web_driver_
+    wait = WebDriverWait(driver, 10)
+    # Scrape the content
+    html_source = driver.page_source
+    # print(html)
 
-# Wait for up to 10 seconds for elements to load
-wait = WebDriverWait(driver, 10)
-
-# Wait for the page to load by waiting for the presence of a specific element
-# element_present = EC.presence_of_element_located(('h2', 'body'))
-# wait.until(element_present)
-
-# Scrape the content
-html_source = driver.page_source
-# print(html)
-
-soup = BeautifulSoup(html_source, 'lxml')
-card = soup.findAll('app-competition-listing',
+    soup = BeautifulSoup(html_source, 'lxml')
+    card = soup.findAll('app-competition-listing',
                     class_='ng-tns-c195-1 ng-star-inserted')
+    
+    f= open("DATA/unstophackathon.txt","w+")
+    for i in card:
+        name = i.h2.text
+        place = i.p.text
+        f.write(f"hackathon name : {name}\n")
+        f.write(f"location/college :{place}\n")
+        print(f'FILE CREATED')
+
+
+    # Close the browser
+    driver.quit()
 
 
 
-for i in card:
-    name=i.h2.text
-    place=i.p.text
-    print(name)
-    print("\n")
-    print(place)
-
-
-
-# Close the browser
-driver.quit()
+if __name__ == '__main__':
+    while True:
+        unstophackathons()
+        time_wait=10
+        print(f"Waiting for {time_wait} minutes")
+        time.sleep(time_wait*60)
