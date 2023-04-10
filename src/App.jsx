8be8fb0caa16cc5926f8e54f7fe74bca32da4f92
scopophobic <adithyanmadhu1234@hmail.com>
2023-04-11 04:29:26 +0530

    change-front-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,7 +8,7 @@ function App() {
     <div className="App">
       <h3>hackUP</h3>
      <h1>COMING SOON</h1>
-     <p>working on backend</p>
+     {/* <p>working on backend</p> */}
     </div>
   )
 }
