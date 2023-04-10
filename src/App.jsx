db9f5f7228b131cff66d584ac417d8594b4632bd
scopophobic <adithyanmadhu1234@hmail.com>
2023-04-11 01:56:36 +0530

    adding_basic_files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h3>hackUP</h3>
+     <h1>COMING SOON</h1>
+     <p>working on backend</p>
+    </div>
+  )
+}
+
+export default App
